70dc6d7b81b31f80fbb388c515989ffdfc8da3bd
xunbu <1036642755@qq.com>
2022-05-19 14:56:44 +0800

    update

--- lightrans176.py
+++ lightrans176.py
@@ -96,7 +96,7 @@ class MainWindow(QWidget):
         self.ui.pushButton_next.setIcon(QIcon(r":/next.png"))
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint) # 窗体总在最前端
         self.ui.textEdit.setPlaceholderText(f"划词翻译:选中要翻译的内容,{hotkey_select}翻译\n输入翻译:输入要翻译的内容,{hotkey_input}翻译\n无换行复制:{hotkey_select}复制无换行符文本\nOCR文字识别:{hotkey_ocr}")
-        self.ui2.label_5.setText('V1.7.6   项目主页: <a style="color:black" href="https://www.xunbu.cc/lightrans">xunbu.cc/lightrans</a>')
+        self.ui2.label_5.setText('V1.7.7   项目主页: <a style="color:black" href="https://www.xunbu.cc/lightrans">xunbu.cc/lightrans</a>')
         self.ui.pushButton_topping.clicked.connect(self.toppingwindow)
         self.ui.pushButton_copy.clicked.connect(self.copytext)
         self.ui.pushButton_setting.clicked.connect(self.showsetting)
